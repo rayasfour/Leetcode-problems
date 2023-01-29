83234ed2d89382c0914c5c2a66140f6f2c11e04e
raghadasfour-02 <r2asfour@uwaterloo.ca>
2023-01-29 13:27:56 -0500

    Leetcode #1 "Two Sum" Solution

--- two_sum.py
+++ two_sum.py
@@ -9,3 +9,10 @@
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# SOLUTION 1 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if nums[i] + nums[i+1] == target:
+                return [i, i+1]

8c7ebc90b93ebcde532cce3ca60ba92645aee4b1
raghadasfour-02 <r2asfour@uwaterloo.ca>
2023-01-29 20:52:48 -0500

    Leetcode #1 "Two Sum" Solution

--- two_sum.py
+++ two_sum.py
@@ -10,9 +10,20 @@
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-# SOLUTION 1 
+# SOLUTION 1 (not entirely correct)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             if nums[i] + nums[i+1] == target:
                 return [i, i+1]
+
+# SOLUTION 2 (HashMap)
+# Time Complexity: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        oldMap = {}
+        for i, n in enumerate(nums):
+            leftover = target - n
+            if leftover in oldMap:
+                return [i, oldMap[leftover]]
+            oldMap[n] = i

a189de37d95a78a5db3d676c075f8e31fcc9a2b6
Raghad Asfour <70734779+raghadasfour-02@users.noreply.github.com>
2023-03-21 01:21:18 -0400

    Leetcode #198 "House Robber" Solution

---
+++ house_robber.py
@@ -0,0 +1,18 @@
+# Using Top-down DP Programming
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+        def dp(i):
+            if i == 0:
+                return nums[i]
+            if i == 1:
+                return max(nums[0], nums[1])
+            
+            if i not in memo:
+                memo[i] = max(dp(i-2)+nums[i], dp(i-1))
+                
+            return memo[i]
+
+        return dp(len(nums) - 1)
+        

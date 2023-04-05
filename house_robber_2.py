bf6c0a6e77dd4258d24b0b1d5c42ec808b149963
Raghad Asfour <70734779+raghadasfour-02@users.noreply.github.com>
2023-04-04 23:59:51 -0400

    Leetcode #198 "House Robber" Solution

---
+++ house_robber_2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        def dp(i: int) -> int:
+            # Base cases
+            if i == 0:
+                return nums[0]
+            elif i == 1:
+                return max(nums[0], nums[1])
+            
+            if i not in memo:
+                # Use recurrence relation to calculate dp[i].
+                memo[i] = max(dp(i - 1), dp(i - 2) + nums[i])
+            
+            return memo[i]
+        
+        memo = {}
+        return dp(len(nums) - 1)

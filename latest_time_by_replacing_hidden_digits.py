d04804a3c10cce20285361992867f9fc737aacea
Raghad Asfour <70734779+raghadasfour-02@users.noreply.github.com>
2023-04-09 23:12:10 -0400

    Leetcode #1736 "Latest Time" Solution

--- latest_time_by_replacing_hidden_digits.py
+++ latest_time_by_replacing_hidden_digits.py
@@ -1,6 +1,7 @@
 class Solution:
     def maximumTime(self, time: str) -> str:
         time = list(time)
+        
         for i in range(len(time)): 
             if time[i] == "?": 
                 if i == 0: time[i] = "2" if time[i+1] in "?0123" else "1"
